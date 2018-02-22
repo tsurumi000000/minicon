1c9b06f6e83de23ea78ab770eca6f62362602b73
tsurumi000000 <25793803+tsurumi000000@users.noreply.github.com>
2018-02-22 09:05:59 +0000

    event_contoller add action

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,14 +1,41 @@
 class Admin::EventsController < ApplicationController
+  layout 'admin'
+  
+  before_action :set_event, only: [:show, :edit, :update]
+  
   def index
     @events = Event.all
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def new
+    @event = Event.new
+  end
+  
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to admin_event_path(@event), notice: 'Event was successfully created.'
   end
 
   def edit
+    @event = Event.find(params[:id])
+  end
+  
+  def update
+    @event.update(event_params)
+    redirect_to admin_event_path(@event), notice: 'Event was successfully updated.'
+  end
+  
+  private
+  def set_event
+    @event = Event.find(params[:id])  
+  end
+  
+  def event_params
+    params.require(:event).permit(:title, :location, :start_at, :ticket_price, :ticket_quantity, :event_category_id)
   end
 end
